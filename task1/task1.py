c293220d66f9de34f03da9540b5e50b13981258d
Aziyat Murzamidinov <aziyatalik4@gmail.com>
2022-06-05 21:26:42 +0900

    adding task1

--- task1/task1.py
+++ task1/task1.py
@@ -1,53 +1,74 @@
 #!/usr/bin/env python
 from darknet_ros_msgs.msg import BoundingBoxes
+from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 import rospy
-
-IMAGE_SIZE_X = 1280
-CRITICAL_BOX_SIZE = 1280 / 10
 
 class task1:
     def __init__(self):
         self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
         self.sub = rospy.Subscriber("/darknet_ros/bounding_boxes", BoundingBoxes, self.callback_fn)
-    
+        self.arm_pub = rospy.Publisher('move_arm', String, queue_size=1)
+        self.rotate = True
+        self.stop = 0
     def callback_fn(self, data):
         for box in data.bounding_boxes:
-            if box.Class == 'bottle':
-
-                '''
-                float64 probability
-                int64 xmin
-                int64 ymin
-                int64 xmax
-                int64 ymax
-                int16 id
-                string Class
-                '''
-
-                '''
-                display size:
-                1280 x 720 pixels
-                '''
-
-                # find middle of bottle's bounding box
-                mid_x = (box.xmin + box.xmax)/2
-                dis = IMAGE_SIZE_X/2 - mid_x
+            if (box.Class == 'bottle') and self.stop <= 2:
+                self.num_detected += 1
+                midImage = 640 #to be configured
+                difBoxes = box.xmax - box.xmin
+                bottle_mid = box.xmin + (difBoxes / 2)
+                threshold = 30
+                velAngular = 0.15
+                vel = 0.15
+                #We will use this for calibration purpose
+                # rospy.loginfo(difBoxes)
+                # rospy.loginfo(box.Class)
 
                 twist = Twist()
-
-                # move toward bottle until the size of its bounding box >= critical size
-                if box.xmax - box.xmin < CRITICAL_BOX_SIZE:
-                    twist.linear.x = 0.2; twist.linear.y = 0.0; twist.linear.z = 0.0
+                twist.linear.x = 0
+                twist.linear.y = 0
+                twist.linear.z = 0
+                twist.angular.x = 0
+                twist.angular.y = 0
+                twist.angular.z = 0
+                if self.num_detected == 1:
+                    self.pub.publish(twist)
+                if midImage > bottle_mid + threshold:
+                    twist.angular.z = velAngular
+                elif midImage < bottle_mid - threshold:
+                    twist.angular.z = -velAngular
                 else:
-                    twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 0.0
+                    twist.angular.z = 0
+				
+                if difBoxes < 100:
+                   twist.linear.x = vel
 
-                # if the center of bounding box is not at screen center with some error range, rotate
-                if abs(dis) > 50:
-                    twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 0.2 if dis > 0 else -0.2
+                elif difBoxes < 200:
+                    twist.linear.x = vel - 0.05
+                elif difBoxes < 290:
+                    twist.linear.x = vel - 0.1
+
                 else:
-                    twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 0.2 if dis > 0 else -0.2
+                    twist.linear.x = 0
+                    self.stop += 1		
+				
+                self.pub.publish(True)
+                #for calibration
+				#rospy.loginfo(box)
                 self.pub.publish(twist)
+                if self.stop==2:
+                    twist.angular.x = 0
+                    twist.angular.y = 0
+                    twist.angular.z = 0
+                    twist.linear.x = 0
+                    twist.linear.y = 0
+                    twist.linear.z = 0
+                    self.pub.publish(twist)
+                    rospy.sleep(0.5)
+                    ##Pick it up
+                    self.arm_pub.publish('G')
+                    self.stop+=1
 
 def main():
     c = task1()
