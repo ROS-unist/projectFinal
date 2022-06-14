cd76b527533318c7cab2e42798157b3a42421696
Azamat Myrzabekov <myrzabekoffazamat@gmail.com>
2022-06-14 18:00:39 +0900

    finished

--- task1/task1.py
+++ task1/task1.py
@@ -2,17 +2,16 @@
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 import moveit_commander
-import moveit_msgs.msg
 import rospy
 
 import sys
 
 IMAGE_SIZE_X = 1280
-CRITICAL_BOX_SIZE = 1280 / 10
+CRITICAL_BOX_SIZE = 355 #1280 / 10
 
 class task1:
     def __init__(self):
-        self.moving = False
+        self.done = False
 
         self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
         self.sub = rospy.Subscriber('converted_message', String, self.callback_fn)
@@ -23,10 +22,7 @@ class task1:
         self.gripper = moveit_commander.MoveGroupCommander('gripper')
         self.arm = moveit_commander.MoveGroupCommander('arm')
         self.arm.set_planning_time(2) # do we need this only for arm?
-
-        # this is only for RVIZ to visualize move_group commands, do we need this then? I think NO
-        # self.pub2 = rospy.Publisher('/move_group/display_planned_path', moveit_msgs.msg.DisplayTrajectory)
-        # rospy.sleep(10)
+        self.gripper.set_planning_time(2)
 
     # destructor to stop the robot at the end
     def __del__(self):
@@ -37,28 +33,20 @@ class task1:
         joint_gripper = self.gripper.get_current_joint_values()
         joint_gripper[0] = v
         self.gripper.go(joints=joint_gripper, wait=True)
-        rospy.sleep(5)
+        # rospy.sleep(5)
         self.gripper.stop()
 
-        # self.gripper.clear_pose_targets() # it is supposed to be used with pose planner
-
-        # display_trajectory = moveit_msgs.msg.DisplayTrajectory()
-        # display_trajectory.trajectory_start = self.robot.get_current_state()
-        # self.pub2.publish(display_trajectory)
+        # self.arm.clear_pose_targets() # it is supposed to be used with pose planner
 
     def move_arm(self, v):
         joint_values = self.arm.get_current_joint_values() 
         joint_values[0] = v[0]; joint_values[1] = v[1]; joint_values[2] = v[2]; joint_values[3] = v[3]
 
         self.arm.go(joints=joint_values, wait=True)
-        rospy.sleep(5)
+        rospy.sleep(10)
         self.arm.stop()
 
         # self.arm.clear_pose_targets() # it is supposed to be used with pose planner
-
-        # display_trajectory = moveit_msgs.msg.DisplayTrajectory()
-        # display_trajectory.trajectory_start = self.robot.get_current_state()
-        # self.pub2.publish(display_trajectory)
 
     def stop_robot(self):
         self.moving = False
@@ -72,7 +60,7 @@ class task1:
         self.move_gripper(0.010)
 
         # arm forward
-        self.move_arm([0.0, 0.0, 0.0, 0.0])
+        self.move_arm([0.0, 1.02, -0.4, -0.4538])
 
         # close gripper 
         self.move_gripper(-0.010)
@@ -80,17 +68,17 @@ class task1:
         # arm home pose
         self.move_arm([0.0, -1.0, 0.3, 0.7])
 
-        # finish program
-        sys.exit(0)
+        # task is done
+        self.done = True
 
     def move(self,d):
         self.moving = True
 
         twist = Twist()
         if d == 'f':
-            twist.linear.x = 0.05; twist.linear.y = 0.0; twist.linear.z = 0.0
+            twist.linear.x = 0.1; twist.linear.y = 0.0; twist.linear.z = 0.0
         elif d == 'b':
-            twist.linear.x = -0.05; twist.linear.y = 0.0; twist.linear.z = 0.0
+            twist.linear.x = -0.1; twist.linear.y = 0.0; twist.linear.z = 0.0
         
         self.pub.publish(twist)
 
@@ -108,18 +96,21 @@ class task1:
 
     # TODO something is not correct here :cry
     def callback_fn(self, s):
+        if self.done == True:
+            return
+
         m = s.data.split()
-        if m[0] == 'bottle': # or m[0] == 'vase': # msg converter sends only this msgs but anyway check it 
+        if m[0] == 'bottle' or m[0] == 'vase': # msg converter sends only this msgs but anyway check it 
             # find middle of bottle's bounding box
             mid = (int(m[2]) + int(m[1]))/2
 
-            e = 50
+            e = 30
             if mid > IMAGE_SIZE_X / 2 + e or mid < IMAGE_SIZE_X / 2 - e:
                 # if the center of bounding box is not at screen center with some error range, rotate
                 self.fix_target(mid, e)
 
             # move toward bottle until the size of its bounding box >= critical size
-            if int(m[2]) - int(m[1]) < CRITICAL_BOX_SIZE and self.moving == False:
+            if int(m[2]) - int(m[1]) < CRITICAL_BOX_SIZE:
                 self.move('f')
             else:
                 self.stop_robot()
